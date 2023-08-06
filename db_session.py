4db0fb846923e9ffb55c68a6d128c9ce161567c3
Jannis15 <78983365+Jannis15@users.noreply.github.com>
2023-08-06 03:11:27 +0200

    prod url

--- db_session.py
+++ db_session.py
@@ -5,16 +5,16 @@ from sqlalchemy.orm import sessionmaker
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./eSportsCalendar.db"
 
 # PostgreSQL connection parameters
-DB_USER = "postgres"
-DB_PASSWORD = "admin"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "esports"
+# DB_USER = "postgres"
+# DB_PASSWORD = "admin"
+# DB_HOST = "localhost"
+# DB_PORT = "5432"
+# DB_NAME = "esports"
 
 
 # Construct the PostgreSQL database URL
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:NcZdQtivYG8VWFkOsb7S@containers-us-west-148.railway.app:6090/railway"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
