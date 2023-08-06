fff571c871c8fff4785d8ea474124dad892e1791
Jannis15 <78983365+Jannis15@users.noreply.github.com>
2023-08-06 03:31:33 +0200

    env file

--- db_session.py
+++ db_session.py
@@ -1,20 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./eSportsCalendar.db"
-
 # PostgreSQL connection parameters
 # DB_USER = "postgres"
 # DB_PASSWORD = "admin"
 # DB_HOST = "localhost"
 # DB_PORT = "5432"
 # DB_NAME = "esports"
-
-
-# Construct the PostgreSQL database URL
 # SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:NcZdQtivYG8VWFkOsb7S@containers-us-west-148.railway.app:6090/railway"
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
